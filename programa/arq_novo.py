9d123f62fecf69377dcb0aec98679a908aa16ebd
Claudio G. Vargas <claudio.g.vargas@gmail.com>
2022-08-09 10:53:05 -0300

    Adicionando arquivo com função de soma!

---
+++ programa/arq_novo.py
@@ -0,0 +1,10 @@
+def somar(a, b):
+    soma = a + b
+    return soma
+
+
+num1 = int(input('Informe um número: '))
+num2 = int(input('Informe outro número: '))
+print(f'Resultado: {somar(num1, num2)}')
+
+
